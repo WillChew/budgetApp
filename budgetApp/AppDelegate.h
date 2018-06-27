011c6b151673ca58dac854a0ceceadf44df63adb
Erik Goossens <erikmgoossens@gmail.com>
2018-06-27 13:36:59 -0400

    Initial Commit

---
+++ budgetApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  budgetApp
+//
+//  Created by Erik Goossens on 2018-06-27.
+//  Copyright © 2018 Erik Goossens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
