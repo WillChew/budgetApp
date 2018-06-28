a8bc394d2bd116c8b20c7c22b96f9547703d8580
Erik Goossens <erik@eriks-macbook-air.local>
2018-06-28 19:03:04 -0400

    changes RealmManager to DataManager

---
+++ budgetApp/DataManager.h
@@ -0,0 +1,38 @@
+//
+//  RealmManager.h
+//  budgetApp
+//
+//  Created by Erik Goossens on 2018-06-28.
+//  Copyright © 2018 Erik Goossens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Realm/Realm.h"
+
+
+@class Section;
+@class Expense;
+
+@interface DataManager : NSObject
+
+#pragma mark - Realm
+
+- (void)createTestObjects;
+
+- (RLMResults<Section*> *)createSectionObjects;
+
+- (void)saveExpense:(Expense *)expense withSection:(NSString *)sectionTitle;
+
+- (RLMArray<Section*>*)fetchAllSections;
+
+- (void)reset;
+
+#pragma mark - UserDefaults
+
+// 2 methods
+
+// 1. saveBudget: -> Void
+// 2. getBudget -> NSNumber
+
+
+@end
