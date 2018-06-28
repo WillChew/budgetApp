04014c9982bfdb21a02cab85f1db345df197158a
Erik Goossens <erik@eriks-macbook-air.local>
2018-06-28 17:51:18 -0400

    adds save expense

--- budgetApp/AppDelegate.m
+++ budgetApp/AppDelegate.m
@@ -14,6 +14,7 @@
 
 @interface AppDelegate ()
 @property (nonatomic) RLMRealm *realm;
+@property (nonatomic) RLMResults<Section*>* sections;
 @end
 
 @implementation AppDelegate
@@ -23,44 +24,49 @@
     // Override point for customization after application launch.
     self.realm = [RLMRealm defaultRealm];
 //    [self createSectionObjects];
-    RLMResults<Section*> *results = [self createSectionObjects];
+    self.sections = [self createSectionObjects];
     
     // init an expense and call the save method below to test it
     // fetch all sections method, call it from here to test it.
     
     // test reset
-    NSLog(@"count: %@", @(results.count));
+    NSLog(@"count: %@", @(self.sections.count));
+//    [self createTestObjects];
     
     return YES;
 }
 
 - (void)createTestObjects {
     
+    Section *food = self.sections[0];
+    Expense *expense1 = [[Expense alloc] initWithName:@"Ice cream" amount:[[NSDecimalNumber alloc]initWithInt:10] description:@"Campo on King" date:[NSDate date]];
+    Expense *expense2 = [[Expense alloc] initWithName:@"Fruit" amount:[[NSDecimalNumber alloc]initWithInt:10] description:@"Fresh and Wild" date:[NSDate date]];
     
-//    Section *section1 = [[Section alloc] init];
-//    section1.title = @"Food";
-//
-//    Expense *expense1 = [[Expense alloc] initWithName:@"Ice cream" amount:[[NSDecimalNumber alloc]initWithInt:10] description:@"Campo on King" date:[NSDate date]];
-//
-//    [section1.expenses addObject:expense1];
+    Section *entertainment = self.sections[1];
+    Expense *expense3 = [[Expense alloc] initWithName:@"Movie night" amount:[[NSDecimalNumber alloc]initWithInt:28] description:@"Went to see Deadpool 2" date:[NSDate date]];
+    Expense *expense4 = [[Expense alloc] initWithName:@"Bar hopping with friends" amount:[[NSDecimalNumber alloc]initWithInt:80] description:@"Drinks are on me" date:[NSDate date]];
     
-//    Section *section2 = [[Section alloc] init];
-//    section2.title = @"Food";
-//
-//    Expense *expense2 = [[Expense alloc] initWithName:@"Fruit" amount:[[NSDecimalNumber alloc]initWithInt:10] description:@"Fresh and Wild" date:[NSDate date]];
-//
-//    [section2.expenses addObject:expense2];
-//
-//    [self.realm transactionWithBlock:^{
-//        [self.realm addObject:section2];
-//    } error:nil
-//     ];
-//
-//
-//    Expense *e2 = [[Expense alloc]initWithName:@"bills" amount:[[NSDecimalNumber alloc]initWithInt:20] description:@"paid bills" date:[NSDate date]];
-//    Section *s2 = [[Section alloc] init];
-//    s2.title = @"Fun";
-//    e2.section = s2;
+    Section *utility = self.sections[2];
+    Expense *expense5 = [[Expense alloc] initWithName:@"Internet bill" amount:[[NSDecimalNumber alloc]initWithInt:92] description:@"High speed fibre optic" date:[NSDate date]];
+    Expense *expense6 = [[Expense alloc] initWithName:@"Hydro bill" amount:[[NSDecimalNumber alloc]initWithInt:55] description:@"For the current month" date:[NSDate date]];
+    
+     
+     Section *transportation = self.sections[3];
+     Expense *expense7 = [[Expense alloc] initWithName:@"TTC" amount:[[NSDecimalNumber alloc]initWithInt:127] description:@"Monthly pass" date:[NSDate date]];
+     Expense *expense8 = [[Expense alloc] initWithName:@"Uber ride" amount:[[NSDecimalNumber alloc]initWithInt:21] description:@"For the trip downtown" date:[NSDate date]];
+    
+     
+     Section *miscellaneous = self.sections[4];
+     Expense *expense9 = [[Expense alloc] initWithName:@"Indoor skydiving" amount:[[NSDecimalNumber alloc]initWithInt:163] description:@"iFly Toronto" date:[NSDate date]];
+     Expense *expense10 = [[Expense alloc] initWithName:@"New computer desk purchase" amount:[[NSDecimalNumber alloc]initWithInt:120] description:@"IKEA desk" date:[NSDate date]];
+    
+    [self.realm beginWriteTransaction];
+    [food.expenses addObjects:@[expense1, expense2]];
+    [entertainment.expenses addObjects:@[expense3, expense4]];
+    [utility.expenses addObjects:@[expense5, expense6]];
+    [transportation.expenses addObjects:@[expense7, expense8]];
+    [miscellaneous.expenses addObjects:@[expense9, expense10]];
+    [self.realm commitWriteTransaction];
     
 }
 
@@ -68,8 +74,6 @@
 
 // this could be in the init method of the realm manager
 - (RLMResults<Section*> *)createSectionObjects {
-    // check whether the sections exist. Query for all section, count on the section
-    // if they don't then create and save them
     RLMResults<Section*> *sections = [Section allObjects];
     if (sections.count == 0) {
         Section *food = [[Section alloc] init];
@@ -91,7 +95,20 @@
     return sections;
 }
 
+
+// this method is going to be called by the addtransactionviewcontroller
+// 
 - (void)saveExpense:(Expense *)expense withSection:(NSString *)section {
+    // Query using a predicate string
+    
+    // Query using an NSPredicate
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"title = %@", section];
+    RLMResults<Section *> *results = [Section objectsWithPredicate:pred];
+    Section *currentSection = results[0];
+    [self.realm beginWriteTransaction];
+    [currentSection.expenses addObject: expense];
+    [self.realm commitWriteTransaction];
+    
     // fetch the section using the parameter
     // save the expense object to that section's .expenses property
     
